e6731b0fb383d1e5c1ec144221fd03068d915b7e
victor-vdo <victor.vdo@outlook.com>
2024-04-29 08:48:03 -0300

    Kiss principles

--- Library.KISS/Program.cs
+++ Library.KISS/Program.cs
@@ -1,1 +1,160 @@
-﻿
+﻿public static class LibraryManager
+{
+    private static List<Book> books = new List<Book>();
+    private static List<User> users = new List<User>();
+    private static List<Loan> loans = new List<Loan>();
+
+    public static void Start()
+    {
+        Console.WriteLine("Bem vindo ao sistema de gerenciamento de biblioteca!");
+        while (true)
+        {
+            ShowMainMenu();
+            var option = Console.ReadLine();
+            ProcessOption(option);
+        }
+    }
+
+    private static void ShowMainMenu()
+    {
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("Selecione uma opção abaixo:");
+        Console.WriteLine("1 - Cadastrar um livro");
+        Console.WriteLine("2 - Consultar um livro");
+        Console.WriteLine("3 - Consultar todos os livros");
+        Console.WriteLine("4 - Remover um livro");
+        Console.WriteLine("5 - Cadastrar um usuário");
+        Console.WriteLine("6 - Cadastrar um Empréstimo");
+        Console.WriteLine("7 - Devolver um livro");
+        Console.WriteLine("8 - Sair");
+    }
+
+    private static void ProcessOption(string option)
+    {
+        switch (option)
+        {
+            case "1":
+                BookRegister();
+                break;
+            case "2":
+                SearchBook();
+                break;
+            case "3":
+                ShowAllBooks();
+                break;
+            case "4":
+                RemoveBook();
+                break;
+            case "5":
+                UserRegister();
+                break;
+            case "6":
+                LoanRegister();
+                break;
+            case "7":
+                BookReturn();
+                break;
+            case "8":
+                Environment.Exit(0);
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("Opção inválida, tente novamente!");
+                break;
+        }
+    }
+
+    private static void BookRegister()
+    {
+        // Implementação do registro de livros
+    }
+
+    private static void SearchBook()
+    {
+        // Implementação da busca de livros
+    }
+
+    private static void ShowAllBooks()
+    {
+        // Implementação para mostrar todos os livros
+    }
+
+    private static void RemoveBook()
+    {
+        // Implementação para remover um livro
+    }
+
+    private static void UserRegister()
+    {
+        // Implementação para registrar um usuário
+    }
+
+    private static void LoanRegister()
+    {
+        // Implementação para registrar um empréstimo
+    }
+
+    private static void BookReturn()
+    {
+        // Implementação para devolver um livro
+    }
+}
+
+#region Classes
+
+public abstract class Model
+{
+    public Guid Id { get; set; }
+    public Model()
+    {
+        Id = Guid.NewGuid();
+    }
+}
+
+public class User : Model
+{
+    public User(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public bool Active { get; set; }
+}
+
+public class Loan : Model
+{
+    public Loan(Guid userId, Guid bookId, DateTimeOffset loanDate)
+    {
+        UserId = userId;
+        BookId = bookId;
+        LoanDate = loanDate;
+    }
+
+    public Guid UserId { get; set; }
+    public Guid BookId { get; set; }
+    public DateTimeOffset LoanDate { get; set; }
+    public bool Active { get; set; }
+}
+
+public class Book : Model
+{
+    public Book(string title, string author, string iSBN, int year)
+    {
+        Title = title;
+        Author = author;
+        ISBN = iSBN;
+        Year = year;
+    }
+
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public int Year { get; set; }
+    public bool Active { get; set; }
+}
+
+
+#endregion
